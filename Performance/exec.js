14d6fb22690a9db09f2151915ad5e2400dabc70f
zhanglin <1501103995@qq.com>
2021-06-29 00:37:36 +0800

    add exec

---
+++ Performance/exec.js
@@ -0,0 +1,40 @@
+// let a = 10;
+// function foo(b) {
+//     let a = 2
+//     function baz(c) {
+//         console.log(a + b + c)
+//     }
+//     return baz
+// }
+// let fn = foo(2)
+// fn(3)
+// function getVideo(part, chapter) {
+//     const parts = ['es2016', '工程化', 'vue', 'react', 'node']
+
+//     if (parts) {
+//         console.log('current')
+//         if (chapter > 5) {
+//             console.log('开通会员')
+//         }
+//     } else {
+//         console.log('确定读哪章')
+//     }
+// }
+
+// getVideo('es2016', 6)
+
+
+// function getVideo(part, chapter) {
+//     const parts = ['es2016', '工程化', 'vue', 'react', 'node']
+
+//     if (parts) {
+//         console.log('current')
+//         if (chapter > 5) {
+//             console.log('开通会员')
+//         }
+//     } else {
+//         console.log('确定读哪章')
+//     }
+// }
+
+
